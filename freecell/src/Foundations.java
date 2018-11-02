9ec66edc9b932b95f6c7de40073e336b63a5181e
pusztayj <pusztayj20@mail.wlu.edu>
2018-11-01 22:19:11 -0400

    Finalizes Foundations

--- freecell/src/Foundations.java
+++ freecell/src/Foundations.java
@@ -5,9 +5,9 @@ public class Foundations extends AbstractCell{
 	private List<Card> cards;
 	private Suit type;
 	
-	Foundations(Suit type){
+	public Foundations(Suit type){
 		this.type = type;
-		cards = new ArrayList<Card>();
+		cards = new ArrayList<Card>(13);
 	}
     @Override
     public Iterator<Card> iterator() {
@@ -17,37 +17,18 @@ public class Foundations extends AbstractCell{
 	 * Adds a card to foundation pile with appropriate conditions
 	 */
 	public void add(Card card) {
-		if(cards.size() == 0 && card.getSuit() == type && card.getRank() == 1){
-			cards.add(card);
-			System.out.println(cards);
-		}
-		else if(type == card.getSuit()){
-			Card topOfPile = cards.get(0);
-			if(card.compareTo(topOfPile) == 1) {
-				cards.add(0, card);
-				System.out.println(cards);
-			}
-			else {
-				System.out.println("Card is not one rank higher");
+		if(cards.size() == 0) {
+			if(card.getSuit() == type && card.getRank() == 1) {
+				cards.add(card);
 			}
 		}
-		else {
-			System.out.println("Card not same type");
+		else if(cards.size() > 0) {
+			if(type == card.getSuit()){
+				Card topOfPile = cards.get(cards.size()-1);
+				if(card.compareTo(topOfPile) == 1) {
+					cards.add(card);
+				}
+			}	
 		}
 	}
-    public static void main(String[] args) {
-    	Deck deck = new Deck();
-    	deck.shuffle();
-    	Foundations a = new Foundations(Suit.heart);
-    	for(int i = 0; i < 52; ++i){
-    		Card card = deck.deal();
-    		a.add(card);
-    	}
-    	for (Iterator<Card> iterator = a.cards.iterator(); iterator.hasNext();) {
-			Card card = iterator.next();
-			System.out.println(card);
-		}
-   	
-    }
-
 }
--- freecell/src/Tester.java
+++ freecell/src/Tester.java
@@ -1,21 +1,19 @@
 import java.util.Iterator;
 
 public class Tester {
-	
-}
     public static void main(String[] args) {
     	Deck deck = new Deck();
     	deck.shuffle();
-    	Card one = deck.deal();
-    	Card two = deck.deal();
-    	System.out.println(one);
-    	System.out.println(two);
-    	Foundations a = new Foundations(Suit.heart);   
-    	a.add(one);
-    	a.add(two);
-    	for (Card card : a)
-//    	for (Iterator<Card> iterator = a.cards.iterator(); iterator.hasNext();) {
-//			Card card = iterator.next();
-//			System.out.println(card);
-//		}
+    	Foundations a = new Foundations(Suit.heart);
+    	for(int i = 0; i < 52; ++i){
+    		Card card = deck.deal();
+    		a.add(card);
+    	}
+    	for (Object card: a) {
+			System.out.println(card);
+		}
+   	
+    }	
 }
+
+
