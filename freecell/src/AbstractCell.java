14fec5c556e64094cb7c4aa737d6723d5eb1c753
tonydu97 <dutony97@gmail.com>
2018-11-01 22:59:45 -0400

    Fixed interface and abstractclass methods

--- freecell/src/AbstractCell.java
+++ freecell/src/AbstractCell.java
@@ -1,17 +1,43 @@
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Stack;
 
 public class AbstractCell implements CellInterface {
+	
+	protected List<Card> cards;
+
+	
+	public AbstractCell(){
+		cards = new ArrayList<Card>();
+	}
 
 	 public int size(){
-	 	return this.size();
+	 	return cards.size();
+	 }
+	 
+	 public void add(Card c) {
+		 if (this.canAddTo(c)) {
+			 cards.add(c);
+		 }
+		 
 	 }
 
 	 public Card remove() {
-	    //return stack.remove(stack.size() - 1)
+        throw new UnsupportedOperationException(
+                "Remove not supported");
+
 	 }
 	 public Iterator<Card> iterator() {
 		 return cards.iterator();
-	    }
+	 }
+	 
+	 public boolean canAddTo(Card card){
+		 return false;
+	 }
+	 
+	 public boolean canRemoveFrom(Card card) {
+		 return false;
+	 }
 
 }
--- freecell/src/Foundations.java
+++ freecell/src/Foundations.java
@@ -2,20 +2,44 @@ import java.util.*;
 
 public class Foundations extends AbstractCell{
 	
-	private List<Card> cards;
 	private Suit type;
 	
 	public Foundations(Suit type){
+		super();
 		this.type = type;
-		cards = new ArrayList<Card>(13);
+
 	}
-    @Override
-    public Iterator<Card> iterator() {
-        return cards.iterator();
-    }
-	/**
-	 * Adds a card to foundation pile with appropriate conditions
-	 */
+	
+	public boolean canAddTo(Card card) {
+		if(cards.size() == 0) {
+			if(card.getSuit() == type && card.getRank() == 1) {
+				return true;
+			}
+			else {
+				System.out.println("First card must be Ace of" + type);
+				return false;
+			}
+		}
+		else {
+			if(type == card.getSuit()){
+				Card topOfPile = cards.get(cards.size()-1);
+				if(card.compareTo(topOfPile) == 1) {
+					return true;
+				}
+				else { //if(cards.size() > 0)
+					System.out.println("Next card must be one rank higher than top of pile" );
+					return false;
+				}
+			}
+			else {
+				return false;
+			}
+		}
+	}
+	
+	
+	
+	
 	public void add(Card card) {
 		if(cards.size() == 0) {
 			if(card.getSuit() == type && card.getRank() == 1) {
