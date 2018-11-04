17e174682d616319270d93a18865a0a41ab9bddf
David Babik <davidbabik@davids-mbp-2.ad.wlu.edu>
2018-11-04 13:24:29 -0500

    completed tableau method for canAddTo

--- freecell/src/Tableau.java
+++ freecell/src/Tableau.java
@@ -5,43 +5,25 @@ import java.util.*;
 		public Tableau(Suit type){
 			super();
 		}
+		
+		/**
+	     * Checks tabluea rules that card to add to pile is of
+	     * opposite color and one rank one less than the top card in the pile
+	     * @param card you want to add to the tableau
+	     * @return boolean true if can add that card, otherwise false
+	     */
 		public boolean canAddTo(Card card) {
 			Card topCard = this.cards.get(this.cards.size()-1);
 			if (!(topCard.sameColor(card))) {
-				if (topCard.greaterByOne(card)) {
+				if (card.greaterByOne(topCard)) {
 					return true;
-				}	
-				
+				}		
 			}
 			else {
 				return false;
 			}
 			
 			
-				if(card.getSuit() == type && card.getRank() == 1) {
-					return true;
-				}
-				else {
-					System.out.println("First card must be Ace of" + type);
-					return false;
-				}
-			}
-			else {
-				if(type == card.getSuit()){
-					Card topOfPile = cards.get(cards.size()-1);
-					if(card.compareTo(topOfPile) == 1) {
-						return true;
-					}
-					else { //if(cards.size() > 0)
-						System.out.println("Next card must be one rank higher than top of pile" );
-						return false;
-					}
-				}
-				else {
-					return false;
-				}
-			}
-		}
 		public boolean canRemoveFrom(Card card) {
 			if (! (cards.size() == 0)){
 				return true;
