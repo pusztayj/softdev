9c25b5ddd1faff82da25e8e07579a8166b2ac242
pusztayj <pusztayj20@mail.wlu.edu>
2018-11-04 12:53:48 -0500

    Fixes indentation

--- freecell/src/AbstractCell.java
+++ freecell/src/AbstractCell.java
@@ -4,38 +4,36 @@ import java.util.List;
 import java.util.Stack;
 
 public class AbstractCell implements CellInterface {
-protected List<Card> cards;
+	protected List<Card> cards;
 
-public AbstractCell(){
-cards = new ArrayList<Card>();
-}
+	public AbstractCell(){
+		cards = new ArrayList<Card>();
+	}
 
-public int size(){
-return cards.size();
-}
+	public int size(){
+		return cards.size();
+	}
  
-public void add(Card c) {
-if (this.canAddTo(c)) {
-cards.add(c);
-}
+	public void add(Card c) {
+		if (this.canAddTo(c)) {
+			cards.add(c);
+		}
  
-}
+	}
 
-public Card remove() {
-        throw new UnsupportedOperationException(
-                "Remove not supported");
+	public Card remove() {
+		throw new UnsupportedOperationException("Remove not supported");
+	}
 
-}
-public Iterator<Card> iterator() {
-return cards.iterator();
-}
+	public Iterator<Card> iterator() {
+		return cards.iterator();
+	}
  
-public boolean canAddTo(Card card){
-return false;
-}
+	public boolean canAddTo(Card card){
+		return false;
+	}
  
-public boolean canRemoveFrom(Card card) {
-return false;
-}
-
+	public boolean canRemoveFrom(Card card) {
+		return false;
+	}
 }
