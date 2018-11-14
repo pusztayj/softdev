bf087f12a58f6bfe047acb6b15eec0f120ae487f
David Babik <davidbabik@davids-mbp-2.ad.wlu.edu>
2018-11-14 11:29:38 -0500

    DocString test

--- freecell/src/MultiPanel.java
+++ freecell/src/MultiPanel.java
@@ -14,6 +14,7 @@ import java.awt.*;
 public class MultiPanel extends CellPanel {
     /**
      * Constructor for an empty multipanel, displays a wire frame. 
+     * @args - the cell interface
      */
 	public MultiPanel(CellInterface cell) {
 		super(cell);
